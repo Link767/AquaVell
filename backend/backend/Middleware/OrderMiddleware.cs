9e0e6febd0adfde1e428994d556de9be6d543fd8
Buddha_gog <karalisalexander@yandex.ru>
2025-11-19 15:57:46 +0300

    MidWClassCreate

--- backend/backend/Middleware/OrderMiddleware.cs
+++ backend/backend/Middleware/OrderMiddleware.cs
@@ -1,6 +1,32 @@
-﻿namespace backend.Middleware
+﻿using backend.DataBase;
+using backend.DataBase.Table;
+
+namespace backend.Middleware
 {
     public class OrderMiddleware
     {
+        private readonly RequestDelegate _next;
+        public OrderMiddleware(RequestDelegate _next)
+        {
+            this._next = _next; 
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var request = context.Request;
+            var response = context.Response;
+            var path = request.Path;
+            var method = request.Method;
+            if (path.StartsWithSegments("/order") && method == "POST")
+            {
+                var order = await request.ReadFromJsonAsync<Order>(); //Получаем данные с фронта
+                Console.WriteLine($"Упал заказ\n Name: {order.Name} \t Email: {order.Email} \t TelNum:{order.TelNum}");
+
+                DBWork dBWork = new DBWork(@"Server=(localdb)\MSSQLLocalDB;Database=TestBD;Trusted_Connection=True;TrustServerCertificate=True;");
+                await dBWork.AddOrderAsunc(order.Name, order.Email, order.TelNum);
+
+                //await _next(context);
+            }
+        }
     }
 }
--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,5 +1,6 @@
 using backend.DataBase;
 using backend.DataBase.Table;
+using backend.Middleware;
 
 namespace backend
 {
@@ -10,20 +11,7 @@ namespace backend
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            app.Run(async (context) =>
-            {
-                var request = context.Request;
-                var response = context.Response;
-                var path = request.Path;
-                var method = request.Method;
-                if (path.StartsWithSegments("/order") && method == "POST")
-                {
-                    var order = await request.ReadFromJsonAsync<Order>(); //Получаем данные с фронта
-                    Console.WriteLine($"Упал заказ\n Name: {order.Name} \t Email: {order.Email} \t TelNum:{order.TelNum}");
-                    DBWork dBWork = new DBWork(@"Server=(localdb)\MSSQLLocalDB;Database=TestBD;Trusted_Connection=True;TrustServerCertificate=True;");
-                    await dBWork.AddOrderAsunc(order.Name, order.Email, order.TelNum);
-                }
-            });
+            app.UseMiddleware<OrderMiddleware>();
 
             app.Run();
         }
