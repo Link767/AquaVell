5e24e2cfa0154c0275d686023bdbf2230eca827c
Buddha_gog <karalisalexander@yandex.ru>
2025-12-01 11:30:19 +0300

    RecreateProd

---
+++ aquawell.Core/aquawell.Core/Dto/OrderDto.cs
@@ -0,0 +1,8 @@
+﻿namespace aquawell.Core.Dto;
+
+public class OrderDto
+{
+    public string ClientName { get; set; } = "";
+    public string ClientMail { get; set; } = "";
+    public string ClientPhoneNum { get; set; } = "";
+}
