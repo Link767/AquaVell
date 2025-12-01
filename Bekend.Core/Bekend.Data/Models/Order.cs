19309ec7423d36a162512ea6a30815b01561483a
Buddha_gog <karalisalexander@yandex.ru>
2025-12-01 11:20:51 +0300

    Com

--- Bekend.Core/Bekend.Data/Models/Order.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Bekend.Data.Models;
-
-public class Order
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = "";
-    public string Email { get; set; } = "";
-    public string TelNum { get; set; } = "";
-}
